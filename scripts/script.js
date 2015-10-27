471d8c71a1624b7ff6222ccfac9be92a86d74ee0
julrusak <juliannarusak@gmail.com>
2015-10-26 22:39:26 -0400

    Display text after box slide-up

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 $( ".center-box" ).click(function() {
-  $( this ).slideUp();
+  $(this).slideUp('fast', function() {
+   $("#text-box").html(SampleText);
+  })
 });
+
+
+
+
+var SampleText = "What has happened to me? he thought. It was no dream. His room, a regular human bedroom, only rather too small, lay quiet between the four familiar walls. Above the table on which a collection of cloth samples was unpacked and spread out-Samsa was a commercial traveler-hung the picture which he had recently cut out of an illustrated magazine and put into a pretty gilt frame. It showed a lady, with a fur cap on and a fur stole, sitting upright and holding out to the spectator a huge fur muff into which the whole of her forearm had vanished! Gregor's eyes turned next to the window, and the overcast sky-one could hear rain drops beating on the window gutter-made him quite melancholy. What about sleeping a little longer and forgetting all this nonsense, he thought, but it could not be done, for he was accustomed to sleep on his right side and in his present condition he could not turn himself over. However violently he forced himself towards his right side he always rolled on to his back again. He tried it at least a hundred times, shutting his eyes to keep from seeing his struggling legs, and only desisted when he began to feel in his side a faint dull ache he had never experienced before.";
