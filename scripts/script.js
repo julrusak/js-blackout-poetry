f2d7b5a3f69889c91eb9e642690881d6b0f95bad
julrusak <juliannarusak@gmail.com>
2015-10-27 20:20:23 -0400

    Move text down page

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,6 @@ $(".text-box").delegate("span", "click", function() {
   $(this).toggleClass("blackout");
 });
 
-var SampleText = "What has happened to me? he thought. It was no dream. His room, a regular human bedroom, only rather too small, lay quiet between the four familiar walls. Gregor's eyes turned to the window, and the overcast sky, one could hear rain drops beating on the window gutter made him quite melancholy. What about sleeping a little longer and forgetting all this nonsense, he thought, but it could not be done, for he was accustomed to sleep on his right side and in his present condition he could not turn himself over. However violently he forced himself towards his right side he always rolled on to his back again. He tried it at least a hundred times, shutting his eyes to keep from seeing his struggling legs, and only desisted when he began to feel in his side a faint dull ache he had never experienced before.";
-
 function wrapWords(element) {
     var words = element.split(/ /);
     var newHtml = '';
@@ -22,3 +20,4 @@ function wrapWords(element) {
     return newHtml;
 }
 
+var SampleText = "What has happened to me? he thought. It was no dream. His room, a regular human bedroom, only rather too small, lay quiet between the four familiar walls. Gregor's eyes turned to the window, and the overcast sky, one could hear rain drops beating on the window gutter made him quite melancholy. What about sleeping a little longer and forgetting all this nonsense, he thought, but it could not be done, for he was accustomed to sleep on his right side and in his present condition he could not turn himself over. However violently he forced himself towards his right side he always rolled on to his back again. He tried it at least a hundred times, shutting his eyes to keep from seeing his struggling legs, and only desisted when he began to feel in his side a faint dull ache he had never experienced before.";
